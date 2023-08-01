525169e3f156023a11447d1bba5e78e464848c41
omerk42 <omerkhanak42@gmail.com>
2023-08-01 21:33:06 +0300

    add jwt auth

--- posts/serializers.py
+++ posts/serializers.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.db.models import Q
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import RefreshToken
 from rest_framework import exceptions, serializers
 
 
@@ -32,30 +33,37 @@ class User_SignUp_Serializer(serializers.ModelSerializer):
                 detail="passwords must match")
 
 
-class User_Login_Serializer(TokenObtainPairSerializer):
+class User_Login_Serializer(serializers.ModelSerializer):
+
+    password = serializers.CharField(
+        write_only=True, style={'input_type': 'password'})
+    email = serializers.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("email", "password")
+        extra_kwargs = {'password': {'write_only': True}}
+        required = ('email',)
 
     def validate(self, attrs):
 
         password = attrs.get("password")
-        username = attrs.get("username")
+        email = attrs.get("email")
 
         user = None
-
-        if "@" in username:
-            user = User.objects.filter(Q(email__iexact=username)).first()
-        else:
-            raise exceptions.AuthenticationFailed(
-                detail="please enter valid email")
-
+        user = User.objects.filter(Q(email__iexact=email)).first()
         # raise an authentication failed error if a user with that username or email doesn't exist.
         if user is None:
             raise exceptions.AuthenticationFailed(
                 detail="user does not exist!")
-
         else:
             # check if password matches
             if user.check_password(password):
-                return "user logged in"
+                refresh = RefreshToken.for_user(user)
+                return {
+                    'refresh': str(refresh),
+                    'access': str(refresh.access_token),
+                }
 
             else:
                 raise exceptions.AuthenticationFailed(
--- posts/urls.py
+++ posts/urls.py
@@ -1,7 +1,10 @@
 from posts import views
+from rest_framework_simplejwt.views import TokenVerifyView, TokenRefreshView
 from django.urls import path
 
 urlpatterns = [
     path("login", views.User_Login_Serializer_View.as_view(), name="user-login"),
-    path("signup", views.User_SignUp_Serializer_View.as_view(), name="user-signup")
+    path("signup", views.User_SignUp_Serializer_View.as_view(), name="user-signup"),
+    path("token/verify", TokenVerifyView.as_view(), name='token_verify'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
